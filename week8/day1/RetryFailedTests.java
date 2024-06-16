7386655c182a2dc2d57dd8df74a6264702c67de4
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-06-16 13:38:57 +0530

    Add files via upload

---
+++ week8/day1/RetryFailedTests.java
@@ -0,0 +1,29 @@
+package base;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.annotations.ITestAnnotation;
+
+public class RetryFailedTests implements IRetryAnalyzer, IAnnotationTransformer{
+	int maxRetry=0; // starting index
+
+	@Override
+	public boolean retry(ITestResult result) {
+		if (maxRetry < 1) {
+			maxRetry++;
+			return true; //retry will continue
+		}
+		
+		return false; // retry will stop
+	}
+	
+	@Override
+	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+		annotation.setRetryAnalyzer(RetryFailedTests.class);
+	}
+
+}
